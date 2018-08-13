aa8bc3d70be14d81a9b1c988292a04b46b938f93
Christopher Manna <cmanna285@gmail.com>
2018-08-13 17:56:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artistNames: "songTitles"}
 
 function updatePlaylist({}, aristName, songTitle) {
-  return Object.assign({},playlist, {beetles: "Hey Jude"})
+  return Object.assign({},playlist, { beetles: "Hey Jude"} )
 }
